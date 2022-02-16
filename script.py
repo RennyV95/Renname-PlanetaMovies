b748f44ac38cd661ba236e06767b8d0326b1f30d
RennyV95 <Renny.V95@gmail.com>
2022-02-16 03:14:41 -0430

    mi primer commit

---
+++ script.py
@@ -0,0 +1,15 @@
+# Cursos Pro Android by Skueletor ©️ 2021
+class script(object):
+    START_TEXT = '¡👋 Hola, mi principal función es renombrar archivos!\n\n<b>¡Envíame cualquier archivo de Telegram y elige la opción que desees!</b>\nEscriba /help para más información.'
+
+    RENAME_403_ERR = 'Perdón, tú no tienes permitido renombrar este archivo'
+    UPGRADE_TEXT = 'Habla con @RennyV95 para negociar 🌚'
+    DOWNLOAD_START = '😌 Intentando descargar a mi base de datos, espere por favor...'
+    UPLOAD_START = '<b>😀 La descarga ha terminado, estoy intentando subir el archivo a Telegram...</b>'
+    AFTER_SUCCESSFUL_UPLOAD_MSG = '<b>Gracias por usarme 🤓</b>\n Por favor, ingresa a nuestro canal oficial si el bot te resulta útil, <a href="https://t.me/PlanetaMovies">🌎 <b>PʟᴀɴᴇᴛᴀMᴏᴠɪᴇs</b> 🌎</a> y deja tu ❤️'
+    SAVED_THUMB = '<b>Miniatura personalizada guardada ✅\n\nPara ver la miniatura actual usa /showthumb\n\nEsta miniatura es permanente, a menos que escribas /delthumb</b>'
+    DEL_THUMB = 'Miniatura borrada con éxito 😌'
+    NO_THUMB = 'No se encontraron miniaturas personalizadas'
+    SAVED_RECVD_DOC_FILE = '<b>Archivo descargado correctamente 😎</b>'
+    CUSTOM_CAPTION_UL_FILE = '<a href="https://t.me/PlanetaMovies">🌎 <b>PʟᴀɴᴇᴛᴀMᴏᴠɪᴇs</b> 🌎</a>'
+    HELP_USER = '<b>El modo de uso del bot es el siguiente:</b>\n\n◽️ <b>1.</b> Envíe una foto para usarla como miniatura personalizada.\n          ▫️<b>1.1.</b> Para ver la miniatura actual usa <code>/showthumb</code>\n          ▫️<b>1.2.</b> Para eliminar la miniatura actual usa  <code>/delthumb</code>\n◽️ <b>2.</b> Envíame cualquier archivo de Telegram.\n◽️ <b>3.</b> Elija la opción adecuada.'
