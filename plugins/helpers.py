b89e3aea6534c65941fc7b0b894845944b95b047
RennyV95 <Renny.V95@gmail.com>
2022-02-16 09:09:39 -0430

    otra edicion 1

--- plugins/helpers.py
+++ plugins/helpers.py
@@ -23,7 +23,7 @@ async def progress_for_pyrogram(
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = "{0}{1} \n\n<b>📊 Porcentaje:</b> {2}%\n".format(
+        progress = "[<code>{0}{1}</code>] \n\n<b>📊 Porcentaje:</b> {2}%\n".format(
             ''.join(["■" for i in range(math.floor(percentage / 5))]),
             ''.join(["□" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
